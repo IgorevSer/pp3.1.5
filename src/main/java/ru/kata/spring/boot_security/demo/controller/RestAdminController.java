71333d286cb1cc0bdb1185d7630d166d28f8f239
SergeyZ31 <zyzin19942019@yandex.ru>
2023-12-22 22:35:21 +0300

    commit

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RestAdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestAdminController.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/admin")
+@PreAuthorize("hasAnyAuthority('ADMIN')")
 public class RestAdminController {
     private final UserService userService;
 
